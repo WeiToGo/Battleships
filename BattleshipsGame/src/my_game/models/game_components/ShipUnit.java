384101306875d8e1c5084a38bcf55e7acee95f2d
Ivo <Ivo@Ivoo-PC>
2014-02-10 17:36:58 -0500

    Discussion quick changes by Ivo.

--- BattleshipsGame/src/my_game/models/game_components/ShipUnit.java
+++ BattleshipsGame/src/my_game/models/game_components/ShipUnit.java
@@ -11,9 +11,17 @@ package my_game.models.game_components;
 public class ShipUnit extends GameObject {
     private int damageLevel; // (0: healthy, 1: damaged, 2: destroyed)
     
-    public ShipUnit(){
+    private Ship ship;
+    
+    public ShipUnit(Ship ship){
+        
+          this.ship = ship;
           this.type = GameObjectType.Ship;
           this.damageLevel = 0;
+    }
+    
+    public Ship getShip() {
+        return ship;
     }
     
     public int getDamageLevel(){
