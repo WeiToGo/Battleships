8b47806c6fee0ce9cd99135c8dab7c956e9bcc38
iparvanov <ivcho_malkiqa@hotmail.com>
2014-03-13 13:27:07 -0400

    Merge

--- BattleshipsGame/src/my_game/models/ships_impl/MineLayer.java
+++ BattleshipsGame/src/my_game/models/ships_impl/MineLayer.java
@@ -55,6 +55,7 @@ public class MineLayer extends Ship {
     public MineLayer(int pid) {
 		super(pid);
 	}
+<<<<<<< HEAD
 
     public int getCannonDamage() {
 	return cannonDamage;
@@ -79,6 +80,9 @@ public class MineLayer extends Ship {
         }
         //return result;
     }  
+=======
+   
+>>>>>>> f016c4a1ce6e3a98113a4cdee43a9abbf3b52744
     
     public boolean layMine(GameObject target) {
         throw new UnsupportedOperationException("Not yet implemented");
