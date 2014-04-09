f238b20a205d18fd90816693c3e74c7d2718ce7e
lanceh <chvn0515@gmail.com>
2014-04-09 08:56:14 -0400

    tests

---
+++ BattleshipsGame/src/my_game/tests/tempTest.java
@@ -0,0 +1,34 @@
+package my_game.tests;
+
+import java.util.ArrayList;
+
+import my_game.models.ships_impl.RadarBoat;
+import my_game.util.ShipDirection;
+import my_game.util.Vector2;
+
+public class tempTest {
+
+	public static void main(String[] args) {
+		ArrayList<Vector2> positions = new ArrayList<Vector2>();
+	    Vector2 p1 = new Vector2(3,1);        
+	    Vector2 p2 = new Vector2(4,1); 
+	    Vector2 p3 = new Vector2(5,1); 
+	    positions.add(p1);
+	    positions.add(p2);
+	    positions.add(p3);
+		
+	    ShipDirection sd = ShipDirection.West;
+	    
+		RadarBoat rb = new RadarBoat(1, positions, sd);
+		
+		System.out.println(rb.getRadarPositions());
+		System.out.println(rb.getRadarRange().toString());
+		
+		rb.turnOnLongRangeRadar();;
+		
+		System.out.println(rb.getRadarPositions());
+		System.out.println(rb.getRadarRange().toString());
+
+	}
+
+}
